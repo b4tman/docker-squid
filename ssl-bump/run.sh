9ffe134a6882edda4e6f4e1f0d84e8cd1050f6d0
Dmitry <b4tm4n@mail.ru>
2025-08-22 11:00:56 +0300

    ssl-bump: force remove pid (fix #154)

--- ssl-bump/run.sh
+++ ssl-bump/run.sh
@@ -7,6 +7,11 @@ if [ ! -d /var/cache/squid/ssl_db ]; then
 	/usr/lib/squid/security_file_certgen -c -s /var/cache/squid/ssl_db -M 4MB
 fi
 
+# force remove pid
+if [ -e /var/run/squid/squid.pid ]; then
+	rm -f /var/run/squid/squid.pid
+fi
+
 # init cache
 /usr/sbin/squid -f "${SQUID_CONFIG_FILE}" --foreground -z
 
